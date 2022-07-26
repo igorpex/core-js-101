15daaaeda36290b70870ff1103c00f920ba5cc21
igorpex <igor.bogdanov@gmail.com>
2022-07-26 19:32:34 +0300

    feat: add getDigitalRoot task solution

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -317,8 +317,13 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = num;
+  do {
+    const numArr = sum.toString().split('').map((string) => Number(string));
+    sum = numArr.reduce((acc, digit) => acc + digit, 0);
+  } while (sum > 9);
+  return sum;
 }
 
 
